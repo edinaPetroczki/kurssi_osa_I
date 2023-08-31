558ea0079bb040f3c6bb98e3d9f0f6db0ff9ea41
edinaPetroczki <petroczki.edina@gmail.com>
2023-08-31 12:14:19 +0300

    added new task pages and changed picture name and captions

--- navbar.php
+++ navbar.php
@@ -5,6 +5,8 @@
 <a class="<?= ($active == 'kuvagalleria') ? 'active':''; ?>" href="kuvagaleria.php">Kuvagalleria</a>
 <a class="<?= ($active == 'rekisteroitymislomake') ? 'active':''; ?>" href="nettikauppalomake.php">Rekisteröityminen</a>
 <a class="<?= ($active == 'verkkosivu') ? 'active':''; ?>" href="verkkosivu.php">Verkkosivu</a>
+<a class="<?= ($active == 'harjoitus2') ? 'active':''; ?>" href="harjoitus2.php">Harjoitus 2</a>
+<a class="<?= ($active == 'navigaatioharjoitus') ? 'active':''; ?>" href="navigaatioharjoitus.php">Navigaatioharjoitus</a>
 <a href="javascript:void(0);" class="icon" onclick="menutoggle()">
 <i class="fa fa-bars"></i>
 </a>
